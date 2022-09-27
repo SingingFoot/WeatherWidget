de44975f73d79a742e691f72b02987b149a7ca9e
“SingingFoot <singingfoot@gmail.comgit config --global user.name SingingFoot>
2022-09-27 20:57:43 +0300

    time added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dateEl = document.getElementById('date');
+const timeEl = document.getElementById('time');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12 : hour;
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+    const minutes = time.getMinutes();
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' +  `<span id="am-pm">${ampm}</span>`
+
+}, 1000);
