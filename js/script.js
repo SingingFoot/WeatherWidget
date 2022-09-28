02735cdc8f8112d3df3bf9b0477da7eb4da08d8f
“SingingFoot <singingfoot@gmail.comgit config --global user.name SingingFoot>
2022-09-28 22:30:11 +0300

    final version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// constants to get DOM elements by their ids
 const dateEl = document.getElementById('date');
 const timeEl = document.getElementById('time');
 const currentWeatherItemsEl = document.getElementById('current-weather-items');
@@ -5,10 +6,12 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
-
+// API key from https://openweathermap.org/
+// const API_KEY = '4af8801fd832240fbb14775b8a41a895';
+// constants to get a day of the week and a month of the year
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
+// function to get actual time and date and to renew them in the browser every second
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
@@ -19,8 +22,74 @@ setInterval(() => {
     const ampm = hour >= 12 ? 'PM' : 'AM';
     const minutes = time.getMinutes();
 
-    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' +  `<span id="am-pm">${ampm}</span>`
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0' + minutes : minutes) + ' ' +  `<span id="am-pm">${ampm}</span>`
 
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month];
 
 }, 1000);
+// function to call API
+getWeatherData();
+function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        let {latitude, longitude} = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=c224d22ba1e7340db0e31d03438fb2c5`).then(res => res.json()).then(data => {
+            console.log(data);
+            showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData(data) {
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N ' + data.lon + 'E'
+
+    currentWeatherItemsEl.innerHTML = 
+    `<div class="weather-item">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+        <div>Pressure</div>
+        <div>${pressure} mbar</div>
+    </div>
+    <div class="weather-item">
+        <div>Wind Speed</div>
+        <div>${wind_speed} km/h</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunrise</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')} </div>
+    </div>
+    <div class="weather-item">
+        <div>Sunset</div>
+        <div>${window.moment(sunset * 1000).format('HH:mm a')} </div>
+    </div>`;
+
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx==0) {
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
+            </div>
+            `
+        }else{
+            otherDayForcast += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
+            </div>
+            `
+        }
+    })
+
+    weatherForecastEl.innerHTML = otherDayForcast;
+}
